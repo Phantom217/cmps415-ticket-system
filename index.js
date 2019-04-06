f2f2f6be218ce6b4ab8169ba0efaff337691972a
Tyler Oalman <tyleroalman@gmail.com>
2019-04-05 23:13:55 -0500

    fix port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 80;
 
 
-app.get('/', (req, res) => {    res.json({message: 'Hello' })})
+app.get('/', (req, res) => {    res.json({message: 'Hello' })});
 
-app.listen(PORT)
+app.listen(PORT, function() {   console.log("Listening on port: " + PORT)});
